754fd9ef3fef92027c7c261a8a3d23774d8fd28e
Aaron Conway <aaronconwayphd@gmail.com>
2019-06-03 15:55:33 -0400

    new logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import { Link } from '../components/link'
-import Logo from '../../static/logo.svg'
+import Logo from '../../static/logo.png'
 
 import classes from '../styles/index.module.sass'
 
@@ -17,6 +17,17 @@ export default ({ data }) => {
     return (
         <Layout isHome>
             <Logo className={classes.logo} aria-label={siteMetadata.title} />
+                        <section>
+                <h1 className={classes.subtitle}>Integrated Approaches to Research Appraisal and Utilization, Part 1</h1>
+                <div className={classes.introduction}>
+                <p>
+                    Hello! Welcome to NUR1027 for the fall 2019 semester.  This webpage will house the weekly presentations of course content with integrated learning activities. It is hoped this format will be an engaging way to present content typically presented 
+                </p>
+                <p>
+                    Now let's get started!
+                </p>
+                </div>
+            </section>
             {chapters.map(({ slug, title, description }) => (
                 <section key={slug} className={classes.chapter}>
                     <h2 className={classes.chapterTitle}>
