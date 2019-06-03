7397b0d955797ab67b95e306ef5fb3ec5ab4b63e
awconway <aaronconwayphd@gmail.com>
2019-06-03 13:54:03 -0400

    Update exc_01_03.R

--- exercises/exc_01_03.R
+++ exercises/exc_01_03.R
@@ -1,7 +1,1 @@
 RISmed::EUtilsSummary("pinkeye", type="esearch", db="pubmed", datetype='pdat', mindate=2000, maxdate=2015, retmax=500)
-
-download.file("https://aconway.dev/NUR1027/references.bib", destfile = "references.bib", method = "auto")
-
-data <- revtools::read_bibliography("references.bib")
-
-results <- revtools::screen_titles(data)
