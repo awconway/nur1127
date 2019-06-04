f5c8950d09f741e3db3dab5b77a51e3c5cadb3c0
Aaron Conway <aaronconwayphd@gmail.com>
2019-06-04 15:57:10 -0400

    fixing test

--- exercises/test_01_03.R
+++ exercises/test_01_03.R
@@ -2,7 +2,7 @@ test <- function() {
     # Here we can either check objects created in the solution code, or the
     # string value of the solution, available as .solution. See the testTemplate
     # in the meta.json for details.
-    if (!grepl("search(nursing[MeSH])", .solution, fixed = TRUE)) {
+    if (!grepl("search("nursing[MeSH]")", .solution, fixed = TRUE)) {
         stop("Are you searching for the correct term?")
     }
     # This function is defined in the testTemplate
