8df851f4fe4c9cd4004087a9120c8fbbd9dbb01b
Aaron Conway <aaronconwayphd@gmail.com>
2019-06-05 09:59:27 -0400

    fixed test

--- exercises/test_01_04.R
+++ exercises/test_01_04.R
@@ -2,7 +2,7 @@ test <- function() {
   # Here we can either check objects created in the solution code, or the
   # string value of the solution, available as .solution. See the testTemplate
   # in the meta.json for details.
-  if (result < 50) {
+  if (result > 50) {
     stop("Are you searching for the correct term?")
   }
   # This function is defined in the testTemplate
