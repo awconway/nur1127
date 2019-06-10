240b0023e78d9ad76619bc48b2a06b1e80f71ae2
Aaron Conway <aaronconwayphd@gmail.com>
2019-06-10 23:39:43 +0000

    hypothesis config

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,7 +60,7 @@ const SEO = ({ title, description }) => (
                 },
                 {
                     name: 'twitter:description',
-                    content: pageDesc,
+                    content: pageDesc,`
                 },
             ]
 
@@ -72,6 +72,11 @@ const SEO = ({ title, description }) => (
                             rel="stylesheet"
                         />
                     )}
+                    window.hypothesisConfig = function () {
+                    return {
+                                "openSidebar": true
+                            };
+                            };
                     <script src="https://hypothes.is/embed.js" async></script>
                 </Helmet>
             )
