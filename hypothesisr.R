613219fb9141f09b4e8283a1e92881a21df29147
Aaron Conway <aaronconwayphd@gmail.com>
2019-06-18 14:45:14 -0400

    pdf annotations download code

--- hypothesisr.R
+++ hypothesisr.R
@@ -6,3 +6,8 @@ chapter1_any <- hs_search(uri = "https://nur1027-fall-2019.netlify.com/", any = 
 chapter1 <- test %>% 
 filter(stringr::str_detect(uri, 'chapter1'))
 
+# annotations of specific pdfs
+pdf <- hs_search(uri = "https://www.aaronconway.info/NUR1027/dobrow.pdf")
+# combine with others in same chapter
+combined  <- rbid(chapter1_any, pdf)
+
