5042aa6bdc574d3a433155d73e8e7dad34bb79fc
Aaron Conway <aaronconwayphd@gmail.com>
2019-06-18 12:46:28 -0400

    use any for chapter search

--- hypothesisr.R
+++ hypothesisr.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(hypothesisr)
-test <- hs_search_all(uri = "https://www.aaronconway.info")
+test <- hs_search(uri = "https://nur1027-fall-2019.netlify.com/")
+## Probably better to match in the search because there is a limit of 200 results
+chapter1_any <- hs_search(uri = "https://nur1027-fall-2019.netlify.com/", any = "chapter1")
 chapter1 <- test %>% 
-filter(stringr::str_detect(uri, 'chapter1') )
+filter(stringr::str_detect(uri, 'chapter1'))
+
